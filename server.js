c56d45888d418795011d906a95c7d0caf20da1c4
Zeeshan Abidi <Zeeshanabidi20@hotmail.com>
2019-03-26 17:53:20 -0700

    Finshed UI, Working on backend

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var app = express();
 
 //Socket.io has two parts, a client side library that runs on the browser, and a server side library for node.js
 app.use(express.static(__dirname));
+
+var messages = [
+    {name: 'Kobe', message : 'Yo'},
+    {name: 'Lebron', message : 'Whaddup'}
+];
+
+// take in request, and then give us reference
+app.get('/messages', (req,res) =>{
+        res.send(messages)           //respond with 'hello' when a GET request is made to the homepage
+});
+
 var server  = app.listen(3000, () => {
     console.log("Server is listening on port",server.address().port);
 });
